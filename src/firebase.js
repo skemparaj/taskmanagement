4f03f0fb53daad9094faf4c98c6ed521a446fb2c
Kemparaj <your_email@gmail.com>
2026-06-12 17:10:46 +0530

    firebase debug

--- src/firebase.js
+++ src/firebase.js
@@ -21,6 +21,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+console.log("API KEY:", import.meta.env.VITE_FIREBASE_API_KEY);
+console.log("AUTH DOMAIN:", import.meta.env.VITE_FIREBASE_AUTH_DOMAIN);
 
 // Initialize Firebase services
 export const auth = getAuth(app);
